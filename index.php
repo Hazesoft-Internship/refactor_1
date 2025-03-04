3d55439f1a10b14ebf989905d59b13d223b0bee8
Bishal Gurung <bishal.gurung@hazesoft.co>
2025-03-04 16:03:48 +0545

    feat

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ function calculateRectangleArea(float $length, float $width): void
     $area = $length * $width;
     echo "Area of rectangle is: " . $area . "<br>";
 }
+//
 
 calculateCircleArea(5);
 calculateRectangleArea(5, 5);
