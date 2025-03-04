6053ee8b7f17b67a2840b7fa3afd8803c48de2a5
Kripesh oli <152187289+Kripesh3@users.noreply.github.com>
2025-03-04 16:46:03 +0545

    Add files via upload

---
+++ main.php
@@ -0,0 +1,29 @@
+<?php
+abstract class Shape {
+    abstract public function area();
+}
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+class Circle extends Shape {
+    private $radius;
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+    public function area() {
+        return pi() * $this->radius * $this->radius;
+    }
+}
+$rectangle = new Rectangle(10, 5);
+echo "Area of Rectangle: " . $rectangle->area() ;
+$circle = new Circle(7);
+echo "\nArea of Circle: " . $circle->area() ;
+?>
