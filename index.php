1f2233429e078d6f581c5dfdd6a81ef96092414d
Prakash Gurung <gurungprakash2468@gmail.com>
2025-03-04 11:04:01 +0000

    Added index.php

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <?php
+echo "Hello, PHP is working! \n";
 
-function calculateCircleArea(float $radius): void
-{
-    $area = pi() * $radius * $radius;
-    echo "Area of circle is: " . $area . "<br>";
+abstract class Shape{
+    public abstract function area();
 }
 
-function calculateRectangleArea(float $length, float $width): void
-{
-    $area = $length * $width;
-    echo "Area of rectangle is: " . $area . "<br>";
-}
+class Circle extends Shape{
+    private $radius;
 
-calculateCircleArea(5);
-calculateRectangleArea(5, 5);
+    public function __construct($radius){
+        $this->radius=$radius;
+    }
+
+    function area(){
+        $area=pi()*$this->radius*$this->radius;
+        return $area;
+    }
+}
+$circle=new Circle(5);
+echo "The area of the circle is ".$circle->area()."\n";
+
+class Reactangle extends Shape{
+    private $length,$breadth;
+
+    public function __construct($length,$breadth){
+        $this->length=$length;
+        $this->breadth=$breadth;
+    }
+    
+    function area(){
+    $area=$this->length*$this->breadth;
+    return $area;
+  }
+}
+$rect=new Reactangle(5,4);
+echo "The area of the reactangle is ".$rect->area()."\n";
+
+?>
+
+
