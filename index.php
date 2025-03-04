4dc5df2b20e33830b3a1dc6a4b06b3a39f846f81
Ayush Tamang <ayushtamang1995@gmail.com>
2025-03-04 16:57:36 +0545

    line spacing

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ include_once "AreaCircle.php";
 include_once "AreaRectangle.php";
 
 $num = 25;
-
-
 $areaC = new AreaCircle($num);
 echo $areaC->area();
 
 $num2 = 30;
-
 $areaR = new AreaRectangle($num, $num2);
 echo $areaR->area();
 
